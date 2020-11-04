148ea3ac0cef219518b37912b703c74c92974b5e
emilrwx <emil.nuutinen@gmail.com>
2020-11-04 22:03:07 +0200

    add blog page

---
+++ pages/blog.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Layout from '../components/layout'
+
+export default function About () {
+  return (
+    <Layout>
+      <Head>
+        <title>Blog</title>
+      </Head>
+      <div>
+        <h1>Blog</h1>
+        <p>Random thoughts about career and life. Sometimes I share my journal writings here too.</p>
+
+      </div>
+    </Layout>
+  )
+}
