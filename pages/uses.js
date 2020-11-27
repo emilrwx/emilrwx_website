911f9e93459e316ed9a25d873b6ec84bb58673e9
emilrwx <me@emilrwx.com>
2020-11-28 00:26:28 +0200

    update content

--- pages/uses.js
+++ pages/uses.js
@@ -16,9 +16,9 @@ export default function About () {
           <li>Some old Fractal Design Case, Intel i5-6500 @ 3.600GHz, 32 GB DDR4, NVIDIA GeForce GTX 1060 3GB, Kingston 480GB & 120GB SSD, Kingston 500GB HDD</li>
           <li>Dual boot Arch Linux and Windows 10</li>
           <li>LG 38WK95C-C Ultra-Wide Monitor</li>
-          <li>Corsair K65 LUX RGB Tenkeyless Keyboard</li>
-          <li>Logitech MX Master 2S Mouse</li>
-          <li>Blue Yeti Blackout Microphone</li>
+          <li>Corsair K65 LUX RGB Tenkeyless keyboard</li>
+          <li>Logitech MX Master 2S mouse</li>
+          <li>Blue Yeti Blackout microphone</li>
         </ul>
 
         <h2>Software</h2>
@@ -29,8 +29,7 @@ export default function About () {
           <li>My current favorite font is <a href='https://www.jetbrains.com/lp/mono/'>JetBrains Mono</a>.</li>
           <li>Discord, Slack & Microsoft Teams for communication.</li>
           <li>Vim & markdown for organizing my thoughts.</li>
-          <li>Firefox for browsing.</li>
-          <li>Spotify for music when I work.</li>
+          <li>Firefox & Lynx for browsing.</li>
           <li><a href='http://jonls.dk/redshift/'>Redshift</a> to save me from headaches during the nights.</li>
           <li>Mendeley to organize research papers.</li>
           <li>Zathura to read them and everything else pdf.</li>
