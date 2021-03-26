f60b1ba40d20e556243a1cd5e0b918ac3d71315b
Emil Nuutinen <emil@emilnuutinen.com>
2021-03-26 18:58:38 +0200

    Move menu responsivity from React to CSS media querys

--- components/header.js
+++ components/header.js
@@ -2,98 +2,50 @@ import React from 'react'
 import Link from 'next/link'
 import styles from '../styles/Header.module.css'
 
-
 class Header extends React.Component {
   constructor (props) {
-    const isBrowser = () => typeof window !== 'undefined'
     super(props)
     this.state = {
-      isMenuOpen: false,
-      windowWidth: isBrowser ? this.innerWidth : 500
+      isMenuOpen: false
     }
     this.handleClick = this.handleClick.bind(this)
   }
 
-  handleResize = (e) => {
-    this.setState({ windowWidth: window.innerWidth });
-   }
-  
-   componentDidMount() {
-    window.addEventListener("resize", this.handleResize);
-   }
-  
-   componentWillUnMount() {
-    window.addEventListener("resize", this.handleResize);
-   } 
-
   handleClick () { this.setState(state => ({ isMenuOpen: !state.isMenuOpen })) }
 
   render () {
-    const { windowWidth } = this.state
-    const isMobile = windowWidth <= 750
-
-    if (isMobile) {
-      return (
-        <div className={styles.container}>
-          <div className={this.state.isMenuOpen ? styles.hidden : null}>
-            <h1 className={styles.title}>
-              <Link href='/'>
-                Emil Nuutinen
-              </Link>
-            </h1>
-            <h2 className={styles.description}>Data Scientist & Experimentalist.</h2>
-          </div>
-          <div className={`${styles.menu} ${this.state.isMenuOpen ? null : styles.hidden}`}>
-            <Link className={styles.menuLink} href='/'>
-              Home
+    return (
+      <div className={styles.container}>
+        <div className={`${styles.titleContainer} ${this.state.isMenuOpen ? styles.hidden : null}`}>
+          <h1 className={styles.title}>
+            <Link href='/'>
+              Emil Nuutinen
             </Link>
-            <Link href='/about'>
-              About
-            </Link>
-            <Link href='/blog'>
-              Blog
-            </Link>
-            <Link href='/uses'>
-              Uses
-            </Link>
-            <a href='https://github.com/emilnuutinen'>Github</a>
-          </div>
-          <div className={`${styles.burger} ${this.state.isMenuOpen ? styles.hamburgerOpen : null}`} onClick={this.handleClick}>
-            <span />
-            <span />
-            <span />
-          </div>
+          </h1>
+          <h2 className={styles.description}>Data Scientist & Experimentalist.</h2>
         </div>
-      )
-    } else {
-      return (
-        <div className={styles.container}>
-            <div className={styles.titleDesktop}>
-              <h1 className={styles.title}>
-                <Link href='/'>
-                  Emil Nuutinen
-                </Link>
-              </h1>
-              <h2 className={styles.description}>Data Scientist & Experimentalist.</h2>
-            </div>
-            <div className={styles.menuDesktop}>
-              <Link className={styles.menuLink} href='/'>
-                Home
-              </Link>
-              <Link href='/about'>
-                About
-              </Link>
-              <Link href='/blog'>
-                Blog
-              </Link>
-              <Link href='/uses'>
-                Uses
-              </Link>
-              <a href='https://github.com/emilnuutinen'>Github</a>
-            </div>
-          </div>
-      )
-    }
+        <div className={`${styles.menu} ${this.state.isMenuOpen ? null : styles.hidden}`}>
+          <Link className={styles.menuLink} href='/'>
+            Home
+          </Link>
+          <Link href='/about'>
+            About
+          </Link>
+          <Link href='/blog'>
+            Blog
+          </Link>
+          <Link href='/uses'>
+            Uses
+          </Link>
+          <a href='https://github.com/emilnuutinen'>Github</a>
+        </div>
+        <div className={`${styles.burger} ${this.state.isMenuOpen ? styles.hamburgerOpen : null}`} onClick={this.handleClick}>
+          <span />
+          <span />
+          <span />
+        </div>
+      </div>
+    )
   }
 }
 
