dc0a82129a299f299a619c504ecf1bccc3b4953a
Emil Nuutinen <emil@emilnuutinen.com>
2021-03-26 16:11:37 +0200

    create a hamburger menu

--- components/header.js
+++ components/header.js
@@ -1,17 +1,43 @@
 import React from 'react'
+import Link from 'next/link'
 import styles from '../styles/Header.module.css'
 
 class Header extends React.Component {
-  render () {
-    const { title, description } = this.props
+  constructor (props) {
+    super(props)
+    this.state = { isMenuOpen: false }
 
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick () { this.setState(state => ({ isMenuOpen: !state.isMenuOpen })) }
+  render () {
     return (
-      <>
-        <div className={styles.wrapper}>
-          <h1 className={styles.title}>{title}</h1>
-          <h2 className={styles.description}>{description}</h2>
+      <div className={styles.container}>
+        <div className={this.state.isMenuOpen ? styles.hidden : null}>
+          <h1 className={styles.title}>Emil Nuutinen</h1>
         </div>
-      </>
+        <div className={`${styles.menu} ${this.state.isMenuOpen ? null : styles.hidden}`}>
+          <Link className={styles.menuLink} href='/'>
+            Home
+          </Link>
+          <Link href='/about'>
+            About
+          </Link>
+          <Link href='/blog'>
+            Blog
+          </Link>
+          <Link href='/uses'>
+            Uses
+          </Link>
+          <a href='https://github.com/emilnuutinen'>Github</a>
+        </div>
+        <div className={`${styles.burger} ${this.state.isMenuOpen ? styles.hamburgerOpen : null}`} onClick={this.handleClick}>
+          <span />
+          <span />
+          <span />
+        </div>
+      </div>
     )
   }
 }
