6af16dfa189db84bc583541bf3186fa22561f54d
Emil Nuutinen <emil@emilnuutinen.com>
2021-03-26 16:10:26 +0200

    remove header

--- pages/blog.js
+++ pages/blog.js
@@ -1,40 +1,36 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '@layouts/layout'
-import Header from '../components/header'
 import styles from '../styles/Blog.module.css'
 
 import { getAllPosts } from '@api'
 
 export default function Blog (props) {
   return (
-    <>
-      <Header title='Blog' description='Random ramblings about everything.' />
-      <Layout>
-        <Head>
-          <title>Blog</title>
-        </Head>
-        <main>
-          {props.posts.map(function (post, idx) {
-            return (
-              <div key={idx} className={styles.post}>
-                <h2 className={styles.blogHeader}>
-                  <Link href={'/posts/' + post.slug}>
-                    {post.title}
-                  </Link>
-                </h2>
-                <p className={styles.excerpt}>
-                  {post.excerpt}
-                </p>
-                <small className={styles.date}>
-                  {post.date}
-                </small>
-              </div>
-            )
-          })}
-        </main>
-      </Layout>
-    </>
+    <Layout>
+      <Head>
+        <title>Blog</title>
+      </Head>
+      <main>
+        {props.posts.map(function (post, idx) {
+          return (
+            <div key={idx} className={styles.post}>
+              <h2 className={styles.blogHeader}>
+                <Link href={'/posts/' + post.slug}>
+                  {post.title}
+                </Link>
+              </h2>
+              <p className={styles.excerpt}>
+                {post.excerpt}
+              </p>
+              <small className={styles.date}>
+                {post.date}
+              </small>
+            </div>
+          )
+        })}
+      </main>
+    </Layout>
   )
 }
 
--- pages/uses.js
+++ pages/uses.js
@@ -1,72 +1,68 @@
 import Head from 'next/head'
 import Layout from '../layouts/layout'
-import Header from '../components/header'
 
 export default function About () {
   return (
-    <>
-      <Header title='Uses' description="What's on my desk?" />
-      <Layout>
-        <Head>
-          <title>Uses</title>
-        </Head>
-        <main>
-          <p>I get often asked what software or hardware I use. So I decided to make a separate page listing everything I use daily.</p>
+    <Layout>
+      <Head>
+        <title>Uses</title>
+      </Head>
+      <main>
+        <p>I get often asked what software or hardware I use. So I decided to make a separate page listing everything I use daily.</p>
 
-          <b>Main Setup</b>
-          <ul>
-            <li>M1 Macbook Pro 13" with 16 GB RAM & 512 GB SSD</li>
-            <li>LG 38WK95C-C Ultra-Wide</li>
-            <li>Apple Magic Keyboard & Trackpad 2</li>
-            <li>Blue Yeti Blackout microphone</li>
-          </ul>
+        <b>Main Setup</b>
+        <ul>
+          <li>M1 Macbook Pro 13" with 16 GB RAM & 512 GB SSD</li>
+          <li>LG 38WK95C-C Ultra-Wide</li>
+          <li>Apple Magic Keyboard & Trackpad 2</li>
+          <li>Blue Yeti Blackout microphone</li>
+        </ul>
 
-          <b>Software (macOS)</b>
-          <ul>
-            <li>Rectangle for window management.</li>
-            <li>Most of my development is done with <a href='https://code.visualstudio.com/'>Visual Studio Code</a>. I have my own <a href='https://github.com/emilnuutinen/fresh-dark'>theme</a> for it.</li>
-            <li>Vim for quick edits.</li>
-            <li>I also write my thoughts in markdown using Vim. I made templates and scripts automating my daily journals and weekly reflections.</li>
-            <li>Terminal client is iTerm2 with <a href='https://ohmyz.sh/'>Oh My ZSH</a> & <a href='https://github.com/denysdovhan/spaceship-prompt'>Spaceship prompt</a>.</li>
-            <li>My favorite font is <a href='https://rubjo.github.io/victor-mono/'>Victor Mono</a>.</li>
-            <li>Matrix (Element), Slack & Microsoft Teams for communication.</li>
-            <li>Firefox for browsing the internet. (Safari & Chrome for testing)</li>
-            <li><a href='https://justgetflux.com/'>f.lux</a> to save me from headaches during the nights.</li>
-            <li>Mendeley to organize research papers.</li>
-          </ul>
+        <b>Software (macOS)</b>
+        <ul>
+          <li>Rectangle for window management.</li>
+          <li>Most of my development is done with <a href='https://code.visualstudio.com/'>Visual Studio Code</a>. I have my own <a href='https://github.com/emilnuutinen/fresh-dark'>theme</a> for it.</li>
+          <li>Vim for quick edits.</li>
+          <li>I also write my thoughts in markdown using Vim. I made templates and scripts automating my daily journals and weekly reflections.</li>
+          <li>Terminal client is iTerm2 with <a href='https://ohmyz.sh/'>Oh My ZSH</a> & <a href='https://github.com/denysdovhan/spaceship-prompt'>Spaceship prompt</a>.</li>
+          <li>My favorite font is <a href='https://rubjo.github.io/victor-mono/'>Victor Mono</a>.</li>
+          <li>Matrix (Element), Slack & Microsoft Teams for communication.</li>
+          <li>Firefox for browsing the internet. (Safari & Chrome for testing)</li>
+          <li><a href='https://justgetflux.com/'>f.lux</a> to save me from headaches during the nights.</li>
+          <li>Mendeley to organize research papers.</li>
+        </ul>
 
-          <b>Desktop Setup</b>
-          <ul>
-            <li>Some old Fractal Design Case, Intel i5-6500 @ 3.600GHz, 32 GB DDR4, NVIDIA GeForce GTX 1060 3 GB, Kingston 480 GB & 120 GB SSD, Kingston 1 TB HDD</li>
-            <li>Dual boot Ubuntu and Windows 10&#185;</li>
-            <li>LG 38WK95C-C Ultra-Wide</li>
-            <li>Corsair K65 LUX RGB Tenkeyless</li>
-            <li>Logitech MX Master 2S</li>
-          </ul>
+        <b>Desktop Setup</b>
+        <ul>
+          <li>Some old Fractal Design Case, Intel i5-6500 @ 3.600GHz, 32 GB DDR4, NVIDIA GeForce GTX 1060 3 GB, Kingston 480 GB & 120 GB SSD, Kingston 1 TB HDD</li>
+          <li>Dual boot Ubuntu and Windows 10&#185;</li>
+          <li>LG 38WK95C-C Ultra-Wide</li>
+          <li>Corsair K65 LUX RGB Tenkeyless</li>
+          <li>Logitech MX Master 2S</li>
+        </ul>
 
-          <b>Software (Ubuntu)</b>
-          <ul>
-            <li>Pop!_OS Shell for window management.</li>
-            <li>Development environment is the same as with macOS. (VSCode & Vim)</li>
-            <li>Terminal client is Kitty with <a href='https://ohmyz.sh/'>Oh My ZSH</a> & <a href='https://github.com/denysdovhan/spaceship-prompt'>Spaceship prompt</a>.</li>
-            <li>My favorite font is <a href='https://rubjo.github.io/victor-mono/'>Victor Mono</a>.</li>
-            <li>Matrix (Element), Slack & Microsoft Teams for communication.</li>
-            <li>Firefox & Lynx for browsing the internet.</li>
-            <li><a href='http://jonls.dk/redshift/'>Redshift</a> to save me from headaches during the nights.</li>
-            <li>Mendeley to organize research papers.</li>
-            <li>Messy dotfiles can be found <a href='https://github.com/emilnuutinen/dotfiles'>here</a>.</li>
-          </ul>
+        <b>Software (Ubuntu)</b>
+        <ul>
+          <li>Pop!_OS Shell for window management.</li>
+          <li>Development environment is the same as with macOS. (VSCode & Vim)</li>
+          <li>Terminal client is Kitty with <a href='https://ohmyz.sh/'>Oh My ZSH</a> & <a href='https://github.com/denysdovhan/spaceship-prompt'>Spaceship prompt</a>.</li>
+          <li>My favorite font is <a href='https://rubjo.github.io/victor-mono/'>Victor Mono</a>.</li>
+          <li>Matrix (Element), Slack & Microsoft Teams for communication.</li>
+          <li>Firefox & Lynx for browsing the internet.</li>
+          <li><a href='http://jonls.dk/redshift/'>Redshift</a> to save me from headaches during the nights.</li>
+          <li>Mendeley to organize research papers.</li>
+          <li>Messy dotfiles can be found <a href='https://github.com/emilnuutinen/dotfiles'>here</a>.</li>
+        </ul>
 
-          <b>Others</b>
-          <ul>
-            <li>Iphone Xs</li>
-            <li>Bose QC35 II</li>
-            <li>Sony a6000 with Sigma 30mm f/1.4</li>
-          </ul>
+        <b>Others</b>
+        <ul>
+          <li>Iphone Xs</li>
+          <li>Bose QC35 II</li>
+          <li>Sony a6000 with Sigma 30mm f/1.4</li>
+        </ul>
 
-          <small>&#185;My main distro is Ubuntu, but I use Windows for occasional gaming and random stuff that Linux is not the best distro for.</small>
-        </main>
-      </Layout>
-    </>
+        <small>&#185;My main distro is Ubuntu, but I use Windows for occasional gaming and random stuff that Linux is not the best distro for.</small>
+      </main>
+    </Layout>
   )
 }
