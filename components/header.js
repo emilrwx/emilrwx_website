f52174770a0bcf10b49e794f8524a41eadec6d95
Emil Nuutinen <emil@emilnuutinen.com>
2021-03-30 15:49:30 +0300

    change class components to functions & hooks

--- components/header.js
+++ components/header.js
@@ -1,52 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import styles from '../styles/Header.module.css'
 
-class Header extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      isMenuOpen: false
-    }
-    this.handleClick = this.handleClick.bind(this)
-  }
+export default function Header () {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-  handleClick () { this.setState(state => ({ isMenuOpen: !state.isMenuOpen })) }
-
-  render () {
-    return (
-      <div className={styles.container}>
-        <div className={`${styles.titleContainer} ${this.state.isMenuOpen ? styles.hidden : null}`}>
-          <h1 className={styles.title}>
-            <Link href='/'>
-              Emil Nuutinen
-            </Link>
-          </h1>
-          <h2 className={styles.description}>Data Scientist & Experimentalist.</h2>
-        </div>
-        <div className={`${styles.menu} ${this.state.isMenuOpen ? null : styles.hidden}`}>
-          <Link className={styles.menuLink} href='/'>
-            Home
+  return (
+    <div className={styles.container}>
+      <div className={`${styles.titleContainer} ${isMenuOpen ? styles.hidden : null}`}>
+        <h1 className={styles.title}>
+          <Link href='/'>
+            Emil Nuutinen
           </Link>
-          <Link href='/about'>
-            About
-          </Link>
-          <Link href='/blog'>
-            Blog
-          </Link>
-          <Link href='/uses'>
-            Uses
-          </Link>
-          <a href='https://github.com/emilnuutinen'>Github</a>
-        </div>
-        <div className={`${styles.burger} ${this.state.isMenuOpen ? styles.hamburgerOpen : null}`} onClick={this.handleClick}>
-          <span />
-          <span />
-          <span />
-        </div>
+        </h1>
+        <h2 className={styles.description}>Data Scientist & Experimentalist.</h2>
       </div>
-    )
-  }
+      <div className={`${styles.menu} ${isMenuOpen ? null : styles.hidden}`}>
+        <Link className={styles.menuLink} href='/'>
+          Home
+        </Link>
+        <Link href='/about'>
+          About
+        </Link>
+        <Link href='/blog'>
+          Blog
+        </Link>
+        <Link href='/uses'>
+          Uses
+        </Link>
+        <a href='https://github.com/emilnuutinen'>Github</a>
+      </div>
+      <div
+        className={`${styles.burger} ${isMenuOpen ? styles.hamburgerOpen : null}`}
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        <span />
+        <span />
+        <span />
+      </div>
+    </div>
+  )
 }
-
-export default Header
--- layouts/layout.js
+++ layouts/layout.js
@@ -2,21 +2,17 @@ import React from 'react'
 import Header from '../components/header'
 import styles from '../styles/Layout.module.css'
 
-class Layout extends React.Component {
-  render () {
-    const { children } = this.props
+export default function Layout (props) {
+  const { children } = props
 
-    return (
-      <>
-        <Header />
-        <div className={styles.container}>
-          <div className={styles.content}>
-            <main>{children}</main>
-          </div>
+  return (
+    <>
+      <Header />
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <main>{children}</main>
         </div>
-      </>
-    )
-  }
+      </div>
+    </>
+  )
 }
-
-export default Layout
