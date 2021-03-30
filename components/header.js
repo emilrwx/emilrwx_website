2c4449a4d8705b10cafdcb51097fb234bfefa36b
Emil Nuutinen <emil@emilnuutinen.com>
2021-03-30 23:31:33 +0300

    change header to be material-like fixed header with a shadow

--- components/header.js
+++ components/header.js
@@ -6,37 +6,39 @@ export default function Header () {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   return (
-    <div className={styles.container}>
-      <div className={`${styles.titleContainer} ${isMenuOpen ? styles.hidden : null}`}>
-        <h1 className={styles.title}>
-          <Link href='/'>
-            Emil Nuutinen
+    <div className={styles.header}>
+      <div className={styles.content}>
+        <div className={`${styles.titleContainer} ${isMenuOpen ? styles.hidden : null}`}>
+          <h1 className={styles.title}>
+            <Link href='/'>
+              Emil Nuutinen
+            </Link>
+          </h1>
+          <h2 className={styles.description}>Data Scientist & Experimentalist.</h2>
+        </div>
+        <div className={`${styles.menu} ${isMenuOpen ? null : styles.hidden}`}>
+          <Link className={styles.menuLink} href='/'>
+            Home
           </Link>
-        </h1>
-        <h2 className={styles.description}>Data Scientist & Experimentalist.</h2>
-      </div>
-      <div className={`${styles.menu} ${isMenuOpen ? null : styles.hidden}`}>
-        <Link className={styles.menuLink} href='/'>
-          Home
-        </Link>
-        <Link href='/about'>
-          About
-        </Link>
-        <Link href='/blog'>
-          Blog
-        </Link>
-        <Link href='/uses'>
-          Uses
-        </Link>
-        <a href='https://github.com/emilnuutinen'>Github</a>
-      </div>
-      <div
-        className={`${styles.burger} ${isMenuOpen ? styles.hamburgerOpen : null}`}
-        onClick={() => setIsMenuOpen(!isMenuOpen)}
-      >
-        <span />
-        <span />
-        <span />
+          <Link href='/about'>
+            About
+          </Link>
+          <Link href='/blog'>
+            Blog
+          </Link>
+          <Link href='/uses'>
+            Uses
+          </Link>
+          <a href='https://github.com/emilnuutinen'>Github</a>
+        </div>
+        <div
+          className={`${styles.burger} ${isMenuOpen ? styles.hamburgerOpen : null}`}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <span />
+          <span />
+          <span />
+        </div>
       </div>
     </div>
   )
