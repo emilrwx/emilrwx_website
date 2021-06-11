e95ed4870858932fabb77dc603da7153da11aee4
Emil Nuutinen <emil@emilnuutinen.com>
2021-06-11 14:21:55 +0300

    add bio to the left side of the content

--- layouts/layout.js
+++ layouts/layout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from '../components/header'
+import Image from 'next/image'
 import styles from '../styles/Layout.module.css'
 
 export default function Layout (props) {
@@ -9,7 +10,21 @@ export default function Layout (props) {
     <div className={styles.container}>
       <Header />
       <div className={styles.content}>
-        <main>{children}</main>
+        <aside className={styles.bio}>
+          <Image className={styles.profile} src='/me.png' alt='me' width='200' height='200' />
+          <p className={styles.name}><b>Emil Nuutinen</b></p>
+          <p className={styles.description}>Data Scientist & Experimentalist</p>
+          <p className={styles.explainer}><span className={styles.leader}>Location </span>Turku, Finland</p>
+          <p className={styles.explainer}><span className={styles.leader}>Education </span>B.B.A. Business Information Technology, Turku University of Applied Sciences</p>
+          <p className={styles.explainer}><span className={styles.leader}>Interests </span>Data Science, Machine Learning, Software Development, Privacy, Reading, Gym, Drinking too much coffee</p>
+          <p className={styles.explainer}><span className={styles.leader}>This Website </span>If you find any mistakes, bugs or other inappropriateness here, leave an issue or a pull requests to the <a href='https://github.com/emilnuutinen/emilnuutinen_website'>repository</a></p>
+          <p className={styles.explainer}><span className={styles.leader}>Contact </span>hello@emilnuutinen.com</p>
+        </aside>
+        <main className={styles.main}>
+          <div className={styles.children}>
+            {children}
+          </div>
+        </main>
       </div>
     </div>
   )
