c73d79f1ac148bd628c08e6fa11e0689f24bf989
Emil Nuutinen <emil@emilnuutinen.com>
2021-06-11 16:22:19 +0300

    Remove unnecessary import

--- components/header.js
+++ components/header.js
@@ -1,6 +1,5 @@
 import dynamic from 'next/dynamic'
 import Link from 'next/link'
-import Image from 'next/image'
 import styles from '../styles/Header.module.css'
 
 const ThemeToggle = dynamic(() => import('../components/themeToggle'), {
